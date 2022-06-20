792454a9b59acde01bad48280411aee8c50a2d08
mocaris <imocaris@outlook.com>
2022-06-20 15:48:53 +0800

    update

--- lib/flutter_app_lib.dart
+++ lib/flutter_app_lib.dart
@@ -1,7 +1,3 @@
 library flutter_app_lib;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_utils/flutter_utils.dart';
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,8 +1,5 @@
 export 'image_util.dart';
-export 'inputformatter.dart';
-export 'json_util.dart';
 export 'maputil.dart';
 export 'permissionutil.dart';
 export 'sputil.dart';
-export 'timer_util.dart';
 export 'util.dart';
